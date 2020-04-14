1e4e1f1a747eca109ed1a66f9a06813bbe3a78a5
Jon Rowe <hello@jonrowe.co.uk>
2020-04-14 19:07:30 +0100

    Tidy up and refactor package

--- mix.exs
+++ mix.exs
@@ -1,24 +1,17 @@
 defmodule PhoenixLiveViewDropzone.MixProject do
   use Mix.Project
 
+  @version "0.0.2"
+
   def project do
     [
       app: :phoenix_live_view_dropzone,
       description: "A Phoenix LiveView compatible file dropzone.",
-      version: "0.0.1",
       elixir: "~> 1.10",
-      start_permanent: Mix.env() == :prod,
+      version: @version,
       deps: deps(),
-      name: "PhoenixLiveViewDropzone",
-      source_url: "https://github.com/JonRowe/phoenix_live_view_dropzone",
-      package: [
-        licenses: ["MIT"],
-        links: %{}
-      ],
-      docs: [
-        main: "readme",
-        extras: ["README.md"]
-      ]
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -29,6 +22,23 @@ defmodule PhoenixLiveViewDropzone.MixProject do
       {:ex_doc, "~> 0.21.3", only: :dev},
       {:jason, "~> 1.2.0", only: :test},
       {:phoenix_live_view, "~> 0.11.0"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: "https://github.com/JonRowe/phoenix_live_view_dropzone"
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Jon Rowe"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/JonRowe/phoenix_live_view_dropzone"},
+      files: ~w(lib priv) ++ ~w(mix.exs package.json README.md)
     ]
   end
 end
